2c8ffcdfb9c1e3a0e4ce9446574686b4d3b82629
212604718 <anuj.parikh@ge.com>
2017-07-24 21:36:24 -0500

    add courses

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseAction from '../../actions/courseAction';
 
 class CoursesPage extends Component {
 
@@ -24,13 +26,18 @@ class CoursesPage extends Component {
   }
 
   onClickSave() {
-    alert(`Saving ${this.state.course.title}`);
+    this.props.dispatch(courseAction.createCourse(this.state.course));
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
   }
 
   render() {
     return (
     <div>
       <h1>Courses</h1>
+      {this.props.courses.map(this.courseRow)}
       <h2>Add Course</h2>
       <input type="text"
              onChange={this.onTitleChange}
@@ -47,4 +54,15 @@ class CoursesPage extends Component {
   }
 }
 
-export default CoursesPage;
+CoursesPage.propsType = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
